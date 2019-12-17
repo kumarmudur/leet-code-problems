902e051d3b228977d6d04a9a173a67afbe0353ff
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-17 15:47:39 +0530

    missing number example

---
+++ js/array/missingNumber.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let total = nums.length;
+  
+    for(let i = 0; i < nums.length; i++) {
+      total = total + i - nums[i];
+    }
+    return total;
+};
+
+console.log(missingNumber([3, 0, 1]));
